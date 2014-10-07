61336eb2a710251325146b6e7aedc214f5e8a315
Melanie <melanie.stefan@cantab.net>
2014-10-07 10:20:32 -0400

    create parsequizzes.R to parse all quizzes from a given course and extract responses, item correctness and timing information

---
+++ parsequizzes.R
@@ -0,0 +1,37 @@
+## parsequizzes.R
+## parses data from all quizzes in a given directory into three separate tables
+## Written by MI Stefan
+
+# get input directory 
+
+# read in all files from input directory
+
+# make 3 master tables: responses given, item response (1 or 0), time
+
+# go through every file
+ 
+# extract quiz number from file name
+
+# go through every student
+
+# look up student identifier
+
+# add response given to response table
+
+# add item correctness to item response table
+
+# add absolute time to times table
+
+
+
+## save three tables to .xls files
+
+
+
+
+ 
+
+
+
+
+
