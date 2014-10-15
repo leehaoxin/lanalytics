7b5292030d087ecf78a171f030e7ed60d49c13e2
Melanie <melanie.stefan@cantab.net>
2014-10-15 17:54:39 -0400

    new script that reads in absolute timing information to figure out order in which questions were answered and time it took to answer them

---
+++ order_time.R
@@ -0,0 +1,14 @@
+## order_time.R
+## takes timing data created with parsequizzes.R
+## computes order in which questions were answered and absolute time taken
+## Written by MI Stefan
+
+# read in time data
+times <- read.csv(file="times.csv",sep="\t")
+
+# order information per quiz
+
+
+
+# big master table with times to answer each question
+
