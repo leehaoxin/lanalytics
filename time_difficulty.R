d3ca8e89904a9464e2d543ef0df93e1347144f2f
Melanie <melanie.stefan@cantab.net>
2014-10-30 20:51:02 -0400

    make new scrip that will correlate time and difficulty

---
+++ time_difficulty.R
@@ -0,0 +1,7 @@
+## time_difficulty.R
+## takes difficulty data created with parsequizzes.R
+## takes time to answer each question created with order_time.R
+## computes item difficulty and median time for each question
+## correlates them
+## Written by MI Stefan
+
