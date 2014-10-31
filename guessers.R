558d9514acfec042ea23f7f491a35b71ba6f0de6
Melanie <melanie.stefan@cantab.net>
2014-10-31 09:29:15 -0400

    get threhold time

--- guessers.R
+++ guessers.R
@@ -9,22 +9,33 @@
 correctness <- read.csv(file="correctness.csv",sep="\t")
 load(file="./timesToAnswerSec.Rda")
 
-# get rate of correct answers
-rateCorrect = colMeans(correctness[,2:length(correctness)],na.rm = TRUE)
+# again, want to go from quiz 4 to quiz 32
+lowestQuiz = 4
+highestQuiz = 32
 
-# get median time it took to answer each question
-medianTimes <- data.frame(matrix(NA,nrow=1))
-alltimes <- timesToAnswerSec[,2:length(timesToAnswerSec)]
-for (i in 1:ncol(alltimes)){
-    times <- as.matrix(alltimes[,i])
-    suppressWarnings(qMedian <- median(as.numeric(times), na.rm=TRUE))
-    medianTimes <- cbind(medianTimes,qMedian)
+# make table that will hold cheating/guessing info
+# convention: 
+# - time under threshold and correct answer: cheating (1)
+# - time under threshold and wrong anwer: guessing (-1)
+# - time >= threshold or first two or last two questions: 0
+# threshold is median time taken for first two and last two questions 
+# (computed separately for each student and quiz)
+cheating_guessing <- timesToAnswerSec
+cheating_guessing[,2:ncol(cheating_guessing)] = 0
+
+# for each quiz and student take median time of first two and last two questions
+# see questions that were faster than that
+
+for (i in lowestQuiz:highestQuiz){
+    # get indices of relevant columns by finding pattern Q<quiz number>q
+    indices = grep(paste("Q",i,"q",sep=""),colnames(timesToAnswerSec))
+    currentQuiz = timesToAnswerSec[ ,indices]
+    lastQuestion = ncol(currentQuiz)
+    # go through all students
+    for (j in 1:nrow(currentQuiz)){
+    learningQuestions <- currentQuiz[j,c(2,3,lastQuestion-1,lastQuestion)]    
+    learningQuestions <- as.numeric(as.matrix(learningQuestions))
+    threshold <- median(learningQuestions,na.rm=TRUE)        
+        
+    }
 }
-
-medianTimes <- as.matrix(medianTimes[2:length(medianTimes)])
-
-pdf("difficulty_time.pdf")
-plot(medianTimes,rateCorrect,xlab="time [s]",ylab="difficulty [%]",
-     col=rgb(0,100,0,50,maxColorValue=255), pch=16)
-dev.off()
-
