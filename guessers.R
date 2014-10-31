7c3d2cba80c1c9287a020e79d26d466b184cc26a
Melanie <melanie.stefan@cantab.net>
2014-10-31 00:15:36 -0400

    created guessers.R as a copy of difficulty_time.R, since parts of it (reading in data etc.) will be the same

---
+++ guessers.R
@@ -0,0 +1,30 @@
+## guessers.R
+## takes item correctness data created with parsequizzes.R
+## takes time to answer each question created with order_time.R
+## computes proportion of students who guessed
+
+## Written by MI Stefan
+
+# read in correctness and time data (inSeconds)
+correctness <- read.csv(file="correctness.csv",sep="\t")
+load(file="./timesToAnswerSec.Rda")
+
+# get rate of correct answers
+rateCorrect = colMeans(correctness[,2:length(correctness)],na.rm = TRUE)
+
+# get median time it took to answer each question
+medianTimes <- data.frame(matrix(NA,nrow=1))
+alltimes <- timesToAnswerSec[,2:length(timesToAnswerSec)]
+for (i in 1:ncol(alltimes)){
+    times <- as.matrix(alltimes[,i])
+    suppressWarnings(qMedian <- median(as.numeric(times), na.rm=TRUE))
+    medianTimes <- cbind(medianTimes,qMedian)
+}
+
+medianTimes <- as.matrix(medianTimes[2:length(medianTimes)])
+
+pdf("difficulty_time.pdf")
+plot(medianTimes,rateCorrect,xlab="time [s]",ylab="difficulty [%]",
+     col=rgb(0,100,0,50,maxColorValue=255), pch=16)
+dev.off()
+
