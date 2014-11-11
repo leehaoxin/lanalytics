5352e6eee98afcb0b2bab21a6041d47e7da823ca
Melanie <melanie.stefan@cantab.net>
2014-11-11 15:59:43 -0500

    start troubleshooting guesser script; simplified a few loops; still not working as it should though

--- guessers.R
+++ guessers.R
@@ -11,7 +11,8 @@ load(file="./timesToAnswerSec.Rda")
 
 # again, want to go from quiz 4 to quiz 32
 lowestQuiz = 4
-highestQuiz = 32
+highestQuiz = 4
+# highestQuiz = 32
 
 # make table that will hold cheating/guessing info
 # convention: 
@@ -26,16 +27,44 @@ cheating_guessing[,2:ncol(cheating_guessing)] = 0
 # for each quiz and student take median time of first two and last two questions
 # see questions that were faster than that
 
+
+masterIndex = 1
+
 for (i in lowestQuiz:highestQuiz){
     # get indices of relevant columns by finding pattern Q<quiz number>q
     indices = grep(paste("Q",i,"q",sep=""),colnames(timesToAnswerSec))
     currentQuiz = timesToAnswerSec[ ,indices]
     lastQuestion = ncol(currentQuiz)
     # go through all students
-    for (j in 1:nrow(currentQuiz)){
-    learningQuestions <- currentQuiz[j,c(2,3,lastQuestion-1,lastQuestion)]    
-    learningQuestions <- as.numeric(as.matrix(learningQuestions))
-    threshold <- median(learningQuestions,na.rm=TRUE)        
+    # for (j in 1:nrow(currentQuiz)){
+    for(j in 24:24){
+        learningQuestions <- currentQuiz[j,c(2,3,lastQuestion-1,lastQuestion)]    
+        suppressWarnings(learningQuestions <- as.numeric(as.matrix(learningQuestions)))
+        threshold <- median(learningQuestions,na.rm=TRUE) 
+        
+        if(is.na(threshold)){
+            next
+        }
+        
+        for (k in 4:lastQuestion-2){
+            suppressWarnings(qTime <- as.numeric(currentQuiz[j,k]))
+            if ( !is.na(qTime) && (qTime < threshold)){
+                if (is.null(correctness[j,masterIndex+k])){
+                    next
+                }
+                else if (correctness[j,masterIndex+k] == 1){
+                    cheating_guessing[j,masterIndex+k] = 1
+                }   
+                else if (correctness[j,masterIndex+k] ==0){
+                    cheating_guessing[j,masterIndex+k] = -1
+                }                        
+                                        
+            }                            
+                        
+        }
         
     }
+    
+
+    masterIndex <- masterIndex + ncol(currentQuiz)
 }
