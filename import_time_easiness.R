4e9006b90f0a4a926e531c1840b2954193bfeace
Melanie <melanie.stefan@cantab.net>
2014-11-17 12:03:31 -0500

    short new script to be able to work with last year's data

---
+++ import_time_easiness.R
@@ -0,0 +1,6 @@
+## import_time_easiness.R
+## short helper script to import time and easiness information from xlsx
+## (for use with data created earlier in the project with MATLAB)
+## if you are analysing your data from scratch, you will never need this!
+## Written by MI Stefan
+
