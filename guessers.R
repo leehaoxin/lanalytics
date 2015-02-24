b6f60f7f61bbde6d717f97ab274250ffa0deabe9
Melanie <melanie.stefan@cantab.net>
2015-02-24 16:31:01 +0000

    labelled x-axis by quiz number

--- guessers.R
+++ guessers.R
@@ -29,8 +29,9 @@ cheating_guessing[,2:ncol(cheating_guessing)] = 0
 
 
 # make data frame to hold number of questions per quiz
-questions_per_quiz <- data.frame(matrix(NA, nrow=highestQuiz-lowestQuiz+1,ncol=2))
-colnames(questions_per_quiz) <- c('quiz','numberOfQuestions')
+questions_per_quiz <- data.frame(matrix(NA, nrow=highestQuiz-lowestQuiz+1,ncol=3))
+colnames(questions_per_quiz) <- c('quiz','numberOfQuestions','cumulative')
+cumulativeQuestions=0;
 
 
 # make table that will hold all threshold times computed
@@ -47,13 +48,19 @@ for (i in lowestQuiz:highestQuiz){
     indices = grep(paste("Q",i,"q",sep=""),colnames(timesToAnswerSec))
     currentQuiz = timesToAnswerSec[ ,indices]
     lastQuestion = ncol(currentQuiz)
+    
+    # update table of question numbers
+    cumulativeQuestions <- cumulativeQuestions + lastQuestion
+    questions_per_quiz[i-lowestQuiz+1,"quiz"] <- i
+    questions_per_quiz[i-lowestQuiz+1,"numberOfQuestions"] <- lastQuestion
+    questions_per_quiz[i-lowestQuiz+1,"cumulative"] <- cumulativeQuestions
+    
     # go through all students
     for (j in 1:nrow(currentQuiz)){
         learningQuestions <- currentQuiz[j,c(1,2,lastQuestion-1,lastQuestion)]    
         suppressWarnings(learningQuestions <- as.numeric(as.matrix(learningQuestions)))
         # threshold <- median(learningQuestions,na.rm=TRUE)
         suppressWarnings(threshold <- min(learningQuestions,na.rm=TRUE))
-        
         # deal with special case of all learningQuestions being NA
         # (happens if they are not completed)
         if (threshold == Inf){
@@ -98,15 +105,16 @@ png("thresholds_hist.png")
 hist(thresholdList,100,xlab="threshold [s]",main="Guessing thresholds")
 dev.off()
 
+png("cheating_guessing.png",width = 1200, units = "px")
 cheating_guessing_numbers <- as.matrix(cheating_guessing[,2:ncol(cheating_guessing)])
 hmcol<-brewer.pal(3,"RdBu")
 image(t(cheating_guessing_numbers), col=rev(hmcol), axes=FALSE, xlab="quiz", ylab="student")
+tickmarks = (questions_per_quiz[,"cumulative"]-questions_per_quiz[1,"cumulative"])/cumulativeQuestions
+axis(1, labels=lowestQuiz:highestQuiz,  at=tickmarks,cex.axis=1)
+dev.off()
 
 
-# axis(3,at = 1:10, labels=1:10)
 
-# xlab('question')
-# 
 # firstquiz <- cheating_guessing[,2:19]
 # firstquiz_numbers <- as.matrix(firstquiz)
 # hmcol<-brewer.pal(3,"RdBu")
