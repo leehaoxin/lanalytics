7b9640d9d1bc1da01109116e72d805d4088018b4
Melanie I Stefan <melanie.stefan@cantab.net>
2015-05-15 09:28:47 +0100

    compiles with 2013 data, but haven't checked if it works

--- parsequizzes.R
+++ parsequizzes.R
@@ -9,14 +9,16 @@ library(random)
 
 ## Variables (to be specified according to project)
 # input directory
-quizdir = ("/home/melanie/Work/MCM/academy_project/Raw data 2013/Quizzes 4-32 2013 copy")
+quizDir = ("/home/melanie/Work/MCM/academy_project/Raw data 2013/Quizzes 4-32 2013 copy/")
 # year quiz was administered (important for date format conversions later on)
-quizyear = 2013
+quizYear = 2013
 # number of students
 studentNumber = 176;
 # number of first and last quiz (in case it does not start with 1)
-firstquiz=4;
-lastquiz=32;
+firstQuiz=4;
+lastQuiz=32;
+# output directory
+outputDir="./2013_output/" 
 
 # make 3 master tables: responses, item correctness (1 or 0), time
 responses <- data.frame()
@@ -28,7 +30,7 @@ students <- data.frame()
 
 
 # read in all files from input directory
-filenames <- list.files(quizdir, pattern="*.xlsx")
+filenames <- list.files(quizDir, pattern="*.xlsx")
 
 # create random 4-number ids for students; 
 # need to know number of students for that
@@ -36,20 +38,20 @@ filenames <- list.files(quizdir, pattern="*.xlsx")
 studentIDs <- sample(1000:9999, studentNumber, replace = F)
 
 # go through every quiz (need to know number of lowest and highest quiz)
-for (i in firstquiz:lastquiz){
+for (i in firstQuiz:lastQuiz){
     # read in xlsx file - this assumes file names are of the form Quiz3_something.xlsx
     fileindex <- grep(paste("Quiz",i,"_",sep=""),filenames[])
     thisquiz <- filenames[fileindex]
-    file = paste(quizdir,thisquiz,sep="")
-    quizdata = read.xlsx(file,1, as.data.frame=TRUE) 
+    file = paste(quizDir,thisquiz,sep="")
+    quizDirata = read.xlsx(file,1, as.data.frame=TRUE) 
     
              
      # go through every student
-     for (j in 1:nrow(quizdata)){
+     for (j in 1:nrow(quizDirata)){
          # get student e-mail
-         mail = as.character(quizdata[j,1])
+         mail = as.character(quizDirata[j,1])
          # use first quiz to make table of students and id
-         if (i == firstquiz){
+         if (i == firstQuiz){
              id <- studentIDs[j]
              students[j,"id"] <- id
              students[j,"mail"] <- mail
@@ -66,8 +68,8 @@ for (i in firstquiz:lastquiz){
          }
                               
         # go through every column (after the name)
-        for (m in 2:ncol(quizdata)){
-            column = colnames(quizdata)[m]
+        for (m in 2:ncol(quizDirata)){
+            column = colnames(quizDirata)[m]
             if (length(grep("Question.*", column))>0){
                 # make question name according to schema
                 question = substring(column, 10,11)
@@ -78,7 +80,7 @@ for (i in firstquiz:lastquiz){
                                 
                 # add response given to response table
                 if(length(grep(".*response.*",column))>0){
-                    qresponse  = quizdata[j,m]
+                    qresponse  = quizDirata[j,m]
                     # strip response of \n and \t
                     qresponse = gsub("\n","",qresponse)
                     qresponse = gsub("\t","",qresponse)
@@ -88,13 +90,13 @@ for (i in firstquiz:lastquiz){
                 }
                 # add item correctness to item response table
                 else if(length(grep(".*score.*",column))>0){
-                    correctness[index, quizquestion] = quizdata[j,m]                              
+                    correctness[index, quizquestion] = quizDirata[j,m]                              
                 }                
                 # add absolute time to times table                                
                 else if(length(grep(".*responded.*",column))>0){
-                    atime <- strptime(quizdata[j,m],"%Y-%m-%d %H:%M:%S")
+                    atime <- strptime(quizDirata[j,m],"%Y-%m-%d %H:%M:%S")
                     if(!is.na(atime)){
-                        if (atime$year == quizyear-1904){
+                        if (atime$year == quizYear-1904){
                             # this means .xlsx file used 1900 format
                             # add one day because Excel thinks 1900 was a leap year
                             atime$mday <- atime$mday+1
@@ -110,14 +112,14 @@ for (i in firstquiz:lastquiz){
 }
 
 
-### save three tables to .xlsx files
-write.xlsx(responses,file = "responses.xlsx", col.names = TRUE,row.names = FALSE,showNA = TRUE)
-write.xlsx(correctness,file = "correctness.xlsx", col.names = TRUE,row.names = FALSE,showNA=TRUE)
-write.xlsx(times,file = "times.xlsx", col.names = TRUE,row.names = FALSE,showNA=TRUE)
-write.xlsx(students,file = "CONFIDENTIAL_students.xlsx", col.names = TRUE,row.names = FALSE,showNA=TRUE)
+write.xlsx(responses,file = paste(outputDir,"responses.xlsx",sep=''), col.names = TRUE,row.names = FALSE,showNA = TRUE)
+write.xlsx(correctness,file = paste(outputDir,"correctness.xlsx",sep=''), col.names = TRUE,row.names = FALSE,showNA=TRUE)
+write.xlsx(times,file = paste(outputDir,"times.xlsx",sep=''), col.names = TRUE,row.names = FALSE,showNA=TRUE)
+write.xlsx(students,file = paste(outputDir,"CONFIDENTIAL_students.xlsx",sep=''), col.names = TRUE,row.names = FALSE,showNA=TRUE)
 
 ### save responses, correctness and times as .csv as welll
-write.csv(responses,file = "responses.csv")
-write.csv(correctness,file = "correctness.csv")
-write.csv(times,file = "times.csv")
+write.csv(responses,file = paste(outputDir,"responses.csv",sep=''))
+write.csv(correctness,file = paste(outputDir,"correctness.csv",sep=''))
+write.csv(times,file = paste(outputDir,"times.csv",sep=''))
+
 
