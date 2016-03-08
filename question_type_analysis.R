982f3434a8ce09007bb3f38f0cf6db77a32b915e
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-08 13:24:21 +0000

    compute total number of items

--- question_type_analysis.R
+++ question_type_analysis.R
@@ -4,13 +4,23 @@
 # depends on format of user-generated files; won't easily generalise 
 # written by MI Stefan
 
-## load uesful libraries
+# load uesful libraries
 library(xlsx)       # import and export xlsx files
 
-## set working directory
+# set working directory
 setwd("~/Work/MCM/academy_project/lanalytics")
 
-## read input file
+# read input file
 cogLevelData <- read.xlsx("Comparison_of_cognitive_level_2013-2014.xlsx",1)
+
+# check dataset
+head(cogLevelData)
+names(cogLevelData)
+
+# compute total questions for 2013 and 2014 (non-empty question ids)
+totalQuestions2013 <- sum(!is.na(cogLevelData[,"Question.ID.2013"]))
+totalQuestions2014 <- sum(!is.na(cogLevelData[,"MCM.2014.item"]))
+
+# compute total number of quizzes for 2013 and 2014 (using quiz number in question ids)
 
 
