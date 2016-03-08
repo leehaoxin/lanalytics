ce7baa29290a2e41a27eb07eb47f0b1ace94b71c
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-08 12:53:57 +0000

    start script to analyse input to quizzes (numbers of questions of given types etc.)

---
+++ question_type_analysis.R
@@ -0,0 +1,16 @@
+# question_type_analysis.R
+# reads instructor-generated cognitive level file
+# determines total number of quiz questions and questions in different categories
+# depends on format of user-generated files; won't easily generalise 
+# written by MI Stefan
+
+## load uesful libraries
+library(xlsx)       # import and export xlsx files
+
+## set working directory
+setwd("~/Work/MCM/academy_project/lanalytics")
+
+## read input file
+read.xlsx("Comparison of cognitive level 2013-2014 Melanie.xlsx",1)
+
+
