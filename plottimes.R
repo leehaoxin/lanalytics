734aac97945882711315a002826ace207cbb6407
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-08 19:43:15 +0000

    adding 2013 data to time analysis

--- plottimes.R
+++ plottimes.R
@@ -1,33 +1,40 @@
 # plot median total time it took to answer each quiz; compute median across all quizzes
+# to do: compute this for both 2013 and 2014
 
 # load necessary libraries
-library(Hmisc)
 library(xlsx)       # import and export xlsx files
 library(stringr)    # manipulate strings
 library(ggplot2)    # extra plotting capabilities
 library(RColorBrewer) # allows us to use Cynthia Brewer's color schemes
 
+# # load 2013 data
+load(file="2013_output/timestoAnswerSec.Rda")
+
+# load 2014 data
 load("timesToAnswerSec.Rda")
-
-allTotalQuizTimes <- as.data.frame(timesToAnswerSec$id)
-
-lowestQuiz=4
-highestQuiz=32
-
-for(i in lowestQuiz:highestQuiz){
-  thisQuizString <- paste('Q',i,'q', sep='')
-  thisQuizIndex <- grep(thisQuizString,names(timesToAnswerSec))
-  itemTimes <- as.data.frame(lapply(timesToAnswerSec[,thisQuizIndex],as.numeric))
-  quizTimes <- rowSums(itemTimes, na.rm = TRUE)
-  quizString <- paste('Q',i)
-  allTotalQuizTimes[,quizString] <- quizTimes
-}
-
-allTotalQuizTimesMin <- as.matrix(allTotalQuizTimes[,2:ncol(allTotalQuizTimes)]/60)
+timesToAnswerSec2014 <- timesToAnswerSec
+allTotalQuizTimes2014 <- as.data.frame(timesToAnswerSec2014$id)
 
 
-png('totalQuizTimesBoxPlot.png')
-boxplot(allTotalQuizTimesMin,outline=FALSE,main="time to answer quizzes",xlab="Quiz",ylab="min")
-dev.off()
 
-medianQuizTime <- median(allTotalQuizTimesMin)
+# lowestQuiz=4
+# highestQuiz=32
+# 
+# for(i in lowestQuiz:highestQuiz){
+#   thisQuizString <- paste('Q',i,'q', sep='')
+#   thisQuizIndex <- grep(thisQuizString,names(timesToAnswerSec))
+#   itemTimes <- as.data.frame(lapply(timesToAnswerSec[,thisQuizIndex],as.numeric))
+#   quizTimes <- rowSums(itemTimes, na.rm = TRUE)
+#   quizString <- paste('Q',i)
+#   allTotalQuizTimes[,quizString] <- quizTimes
+# }
+# 
+# allTotalQuizTimesMin <- as.matrix(allTotalQuizTimes[,2:ncol(allTotalQuizTimes)]/60)
+# 
+# 
+# png('totalQuizTimesBoxPlot.png')
+# boxplot(allTotalQuizTimesMin,outline=FALSE,main="time to answer quizzes",xlab="Quiz",ylab="min")
+# dev.off()
+# 
+# medianQuizTime <- median(allTotalQuizTimesMin)
+
