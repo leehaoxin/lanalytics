9afff00679969ca3ea90e2797aa4776f132a3b99
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-09 16:06:09 +0000

    compute and plot total time per quiz for 2013, 2014, and both

--- plottimes.R
+++ plottimes.R
@@ -8,7 +8,10 @@ library(ggplot2)    # extra plotting capabilities
 library(RColorBrewer) # allows us to use Cynthia Brewer's color schemes
 
 # # load 2013 data
-load(file="2013_output/timestoAnswerSec.Rda")
+load(file="2013_output/timesToAnswerSec.Rda")
+timesToAnswerSec2013 <- timesToAnswerSec
+allTotalQuizTimes2013 <- as.data.frame(timesToAnswerSec2013$id)
+
 
 # load 2014 data
 load("timesToAnswerSec.Rda")
@@ -16,25 +19,38 @@ timesToAnswerSec2014 <- timesToAnswerSec
 allTotalQuizTimes2014 <- as.data.frame(timesToAnswerSec2014$id)
 
 
+lowestQuiz=4
+highestQuiz=32
 
-# lowestQuiz=4
-# highestQuiz=32
-# 
-# for(i in lowestQuiz:highestQuiz){
-#   thisQuizString <- paste('Q',i,'q', sep='')
-#   thisQuizIndex <- grep(thisQuizString,names(timesToAnswerSec))
-#   itemTimes <- as.data.frame(lapply(timesToAnswerSec[,thisQuizIndex],as.numeric))
-#   quizTimes <- rowSums(itemTimes, na.rm = TRUE)
-#   quizString <- paste('Q',i)
-#   allTotalQuizTimes[,quizString] <- quizTimes
-# }
-# 
-# allTotalQuizTimesMin <- as.matrix(allTotalQuizTimes[,2:ncol(allTotalQuizTimes)]/60)
-# 
-# 
-# png('totalQuizTimesBoxPlot.png')
-# boxplot(allTotalQuizTimesMin,outline=FALSE,main="time to answer quizzes",xlab="Quiz",ylab="min")
-# dev.off()
-# 
+# get data frames of times per quiz
+for(i in lowestQuiz:highestQuiz){
+  thisQuizString <- paste('Q',i,'q', sep='')
+  quizString <- paste('Q',i,sep='')
+    
+  thisQuizIndex2013 <- grep(thisQuizString,names(timesToAnswerSec2013))
+  itemTimes2013 <- as.data.frame(lapply(timesToAnswerSec2013[,thisQuizIndex2013],as.numeric))
+  quizTimes2013 <- rowSums(itemTimes2013, na.rm = TRUE)
+    
+  thisQuizIndex2014 <- grep(thisQuizString,names(timesToAnswerSec2014))
+  itemTimes2014 <- as.data.frame(lapply(timesToAnswerSec2014[,thisQuizIndex2014],as.numeric))
+  quizTimes2014 <- rowSums(itemTimes2014, na.rm = TRUE)
+  
+  allTotalQuizTimes2013[,quizString] <- quizTimes2013
+  allTotalQuizTimes2014[,quizString] <- quizTimes2014  
+}
+
+allTotalQuizTimesMin2013 <- as.matrix(allTotalQuizTimes2013[,2:ncol(allTotalQuizTimes2013)]/60)
+allTotalQuizTimesMin2014 <- as.matrix(allTotalQuizTimes2014[,2:ncol(allTotalQuizTimes2014)]/60)
+
+
+png('totalQuizTimesBoxPlot2013.png')
+boxplot(allTotalQuizTimesMin2013,outline=FALSE,main="time to answer quizzes (2013)",xlab="Quiz",ylab="min")
+dev.off()
+
+png('totalQuizTimesBoxPlot2014.png')
+boxplot(allTotalQuizTimesMin2014,outline=FALSE,main="time to answer quizzes (2014)",xlab="Quiz",ylab="min")
+dev.off()
+
+
 # medianQuizTime <- median(allTotalQuizTimesMin)
 
