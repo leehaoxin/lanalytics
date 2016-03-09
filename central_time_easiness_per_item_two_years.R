00e7724cfda7f1c67c3bfcbe7d95e9eda9bd6512
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-09 16:23:55 +0000

    re-write file description and comments so it relates to easiness only; easier to start with one thing

--- central_time_easiness_per_item_two_years.R
+++ central_time_easiness_per_item_two_years.R
@@ -1,3 +1,7 @@
-## takes easiness and median time data created using easiness_time.R
-## computes averages of item easiness and time for 2 years
+## takes easiness data created using easiness_time.R
+## computes averages of item easiness 2 years
 ## Written by MI Stefan
+
+## import easiness data from both years
+
+## compute mean and standard deviation 
