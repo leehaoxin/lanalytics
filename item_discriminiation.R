81f431b39d5573d8ded359bff5deaa53c8855f5e
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-10 17:46:35 +0000

    item discrimination script

---
+++ item_discriminiation.R
@@ -0,0 +1,5 @@
+# item_discrimination.R
+# comuptes item discrimination for quizzes in 2013 and 2014
+# reports mean item discrimination and standard deviation for both years
+# written by MI Stefan
+
