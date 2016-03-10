e2f20e0b32acaa5e0afa70b89825f407e0dd0f57
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-10 20:31:05 +0000

    remove "2013" from variable names

--- item_discriminiation.R
+++ item_discriminiation.R
@@ -13,7 +13,7 @@ library(xlsx) # read xlsx files
 library(plyr) # allows for sorting of data frames
 
 # read in data 
-correctness2013 <- read.csv("2013_output/correctness.csv")
+correctness <- read.csv("2013_output/correctness.csv")
 
 # # read in data from both years
 # correctness2014 <- read.xlsx("correctness.xlsx",1,as.data.frame=TRUE)
@@ -23,36 +23,36 @@ correctness2013 <- read.csv("2013_output/correctness.csv")
 
 #  
 # remove first columns (holding row numbers)
-correctness2013[,1] <- NULL
+correctness[,1] <- NULL
 
 # compute total score per student and add this as the last column to the data frame
-correctness2013[,"total"] <- rowSums(correctness2013[,2:ncol(correctness2013)],na.rm=TRUE)
+correctness[,"total"] <- rowSums(correctness[,2:ncol(correctness)],na.rm=TRUE)
 
 # sort data frame by total number of answers correct 
-sortedCorrectness2013 <- arrange(correctness2013,total)
+sortedCorrectness <- arrange(correctness,total)
 
 # compute number of students in upper and lower group 
 
-numberOfStudents2013 <- nrow(correctness2013)
-groupSize2013 <- round(numberOfStudents2013*27/100)
+numberOfStudents <- nrow(correctness)
+groupSize <- round(numberOfStudents*27/100)
 
 # make a bottom group and a top group by selecting the first and last groupSize students
-bottomGroup2013 <- sortedCorrectness2013[1:groupSize2013,]
-topGroup2013 <- sortedCorrectness2013[(numberOfStudents2013-groupSize2013+1):numberOfStudents2013,]
+bottomGroup <- sortedCorrectness[1:groupSize,]
+topGroup <- sortedCorrectness[(numberOfStudents-groupSize+1):numberOfStudents,]
 
 # make new data frame to hold item discrimination for each item
-itemDiscrimination2013 <- as.data.frame(matrix(nrow = 0, ncol = ncol(correctness2013)))
-names(itemDiscrimination2013)=names(correctness2013)
-itemDiscrimination2013[,1]<-NULL
+itemDiscrimination <- as.data.frame(matrix(nrow = 0, ncol = ncol(correctness)))
+names(itemDiscrimination)=names(correctness)
+itemDiscrimination[,1]<-NULL
 
 # now comes the actual item discrimination computing
 
  
-for (i in 2:ncol(correctness2013)){
+for (i in 2:ncol(correctness)){
   topScore <- sum(topGroup[,i])
   bottomScore <- sum(bottomGroup[,i])
-  itemDiscrimination <- (topScore-bottomScore)/groupSize2013
-  # itemDiscrimination2013[1,i] <- itemDiscrimination
+  itemDiscrimination <- (topScore-bottomScore)/groupSize
+  # itemDiscrimination[1,i] <- itemDiscrimination
 }
 
 
