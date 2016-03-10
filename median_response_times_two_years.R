b678c8efef23f6f030a58289083ca6011db0b142
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-10 16:27:37 +0000

    load data

--- median_response_times_two_years.R
+++ median_response_times_two_years.R
@@ -1,2 +1,14 @@
 # median_response_times_two_years.R
 # compute median of all response times across all students for both years (2013 and 2014)
+
+# # load 2013 data; convert to matrix
+load(file="2013_output/timesToAnswerSec.Rda")
+timesToAnswerSec2013 <- as.matrix(timesToAnswerSec)
+
+# load 2014 data; convert to matrix
+load("timesToAnswerSec.Rda")
+timesToAnswerSec2014 <- as.matrix(timesToAnswerSec)
+
+allTimesToAnswer <- c(timesToAnswerSec2013,timesToAnswerSec2014)
+
+
