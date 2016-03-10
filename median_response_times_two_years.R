9b9d9ffd23d2999f7008c1282c5f61e3ee440f62
Melanie I Stefan <melanie.stefan@cantab.net>
2016-03-10 17:33:21 +0000

    process data so the median can be taken

--- median_response_times_two_years.R
+++ median_response_times_two_years.R
@@ -1,14 +1,25 @@
 # median_response_times_two_years.R
 # compute median of all response times across all students for both years (2013 and 2014)
 
-# # load 2013 data; convert to matrix
+# # load 2013 data; convert to matrix; remove NA
 load(file="2013_output/timesToAnswerSec.Rda")
-timesToAnswerSec2013 <- as.matrix(timesToAnswerSec)
+
+# ignore first column; make into numeric; linearize
+timesToAnswerSec2013 <- timesToAnswerSec[,2:ncol(timesToAnswerSec)]
+timesToAnswerSec2013 <- lapply(timesToAnswerSec2013,as.numeric)
+timesToAnswerSec2013 <- unlist(timesToAnswerSec2013)
 
 # load 2014 data; convert to matrix
 load("timesToAnswerSec.Rda")
-timesToAnswerSec2014 <- as.matrix(timesToAnswerSec)
+timesToAnswerSec2014 <- as.matrix(timesToAnswerSec[2:nrow(timesToAnswerSec),])
+# ignore first column; make into numeric; linearize
+timesToAnswerSec2014 <- timesToAnswerSec[,2:ncol(timesToAnswerSec)]
+timesToAnswerSec2014 <- lapply(timesToAnswerSec2014,as.numeric)
+timesToAnswerSec2014 <- unlist(timesToAnswerSec2014)
 
-allTimesToAnswer <- c(timesToAnswerSec2013,timesToAnswerSec2014)
-
-
+allTimesToAnswerSec <- c(timesToAnswerSec2013,timesToAnswerSec2014)
+ 
+# medianTimeToAnswerSec2013 <- median(timesToAnswerSec2013,na.rm=TRUE)
+# medianTimeToAnswerSec2014 <- median(timesToAnswerSec2014,na.rm=TRUE)
+# medianTimeToAnswerSecAll <- median(allTimesToAnswerSec,na.rm=TRUE)
+# 
