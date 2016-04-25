403fa1d6ef42fc93e879dd340f592d0338401c65
Melanie I Stefan <melanie.stefan@cantab.net>
2016-04-25 17:03:17 +0100

    save change data into data frame

--- items_change.R
+++ items_change.R
@@ -12,10 +12,6 @@ compare_2013_2014 <- compare_2013_2014_raw[compare_2013_2014_raw[,'question_13']
 ## prediction: if nothing happens, star should be the same in all directions
 ## if there is improvement, star should go towards the top left (?)
 
-plot(NA, xlim=c(-4,4), ylim=c(-0.5,0.5),xlab="time change", ylab="easiness change", 
-     main="Item changes 2013-2014")
-
-min_easiness_change = 0
 
 for (i in 1:nrow(compare_2013_2014)){
   easiness_change = compare_2013_2014[i,"easiness_14"]-compare_2013_2014[i,"easiness_13"]
@@ -25,4 +21,15 @@ for (i in 1:nrow(compare_2013_2014)){
   segments(0, 0, time_change, easiness_change)
 }
 
+# plot(NA, xlim=c(-4,4), ylim=c(-0.5,0.5),xlab="time change", ylab="easiness change", 
+#      main="Item changes 2013-2014")
+# 
+# for (i in 1:nrow(compare_2013_2014)){
+#   easiness_change = compare_2013_2014[i,"easiness_14"]-compare_2013_2014[i,"easiness_13"]
+#   time_change = compare_2013_2014[i,"time_14"]-compare_2013_2014[i,"time_13"]
+#   compare_2013_2014[i,"easiness_change"] <- easiness_change
+#   compare_2013_2014[i,"time_change"] <- time_change
+#   segments(0, 0, time_change, easiness_change)
+# }
+# 
 
