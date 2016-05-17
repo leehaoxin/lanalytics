1690faccaa13ed598427651306a073b40d8f7c8b
Melanie I Stefan <melanie.stefan@cantab.net>
2016-05-17 13:34:36 +0100

    start reading in nature of change for items that changed

--- items_change.R
+++ items_change.R
@@ -5,6 +5,7 @@
 
 library(RColorBrewer) # allows us to use Cynthia Brewer's color schemes
 library(ggplot2)    # extra plotting capabilities
+library(xlsx)
 
 compare_2013_2014_raw <- read.csv("2013_2014_compare/time_level_easiness_2013_2014.csv")
 
@@ -15,6 +16,7 @@ compare_2013_2014 <- compare_2013_2014_raw[compare_2013_2014_raw[,'question_13']
 ## prediction: if nothing happens, star should be the same in all directions
 ## if there is improvement, star should go towards the top left (?)
 
+## General plots first
 
 for (i in 1:nrow(compare_2013_2014)){
   easiness_change = compare_2013_2014[i,"easiness_14"]-compare_2013_2014[i,"easiness_13"]
@@ -53,10 +55,25 @@ plot <- ggplot() +
 print(plot)
 dev.off()
 
-# plot(NA, xlim=c(-5,5), ylim=c(-0.5,0.5),xlab="time change", ylab="easiness change", 
-#      main="Item changes 2013-2014")
-# with(compare_2013_2014, mapply("segments", 0, 0, time_change, easiness_change, col=level_13))
+## Plots by change and nature of change
 
+## read in insrtructor file with question changes
+itemsChange <- read.xlsx("Comparison_of_cognitive_level_2013-2014.xlsx",1)
+
+# for all entries in our compare dataframe, read corresponding entry from itemsChange,
+# add columns related to change
+
+
+
+# for (i in 1:nrow(compare_2013_2014)) {
+#   quiz = compare_2013_2014[i,"quiz_13"]
+#   question = compare_2013_2014[i,"question_13"]
+#   q_String = paste("2013_Quiz",quiz,"_q",question,sep="")  
+#   changes = itemsChange[grep(q_String,itemsChange$Question.ID.2013),11:23]
+# #  compare_2013_2014
+# }
+# 
+# 
 
 
 
